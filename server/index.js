34d1b15fa72d67034df99354028294b1dad28dce
Dmitry <dmitryscaletta@mail.ru>
2017-01-05 20:10:55 +0300

    Add link to localhost

--- server/index.js
+++ server/index.js
@@ -214,5 +214,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(3000, () => {
-	console.log('Football Champ app listening on port 3000!')
+	console.log('Football Champ app listening on port http://localhost:3000!')
 })
